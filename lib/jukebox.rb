ebe3ff5177cd2adc83a8554559f4049cc98c37da
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-30 22:14:47 +0000

    Automatically backed up by Learn

---
+++ lib/jukebox.rb
@@ -0,0 +1,32 @@
+require "pry"
+
+def help
+  
+end
+
+def play
+  
+end
+
+def list
+  
+end
+
+def exit_jukebox
+  
+end
+
+def run
+  
+  #prompt user
+  #user input
+  case input
+    when play
+     
+    when list
+       
+    when exit
+         
+    else
+  end
+end
